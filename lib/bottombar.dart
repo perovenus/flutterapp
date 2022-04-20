dc77632512c5426c781c5ea3aa7a9676d1ffae6d
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-04-20 17:27:29 +0700

    update

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -19,7 +19,7 @@ class _NavbarState extends State<Navbar> {
     ];
     void OpenPage(int index) {
         setState(() {
-            selectedIndex = index;
+          selectedIndex = index;
         });
     }
 
